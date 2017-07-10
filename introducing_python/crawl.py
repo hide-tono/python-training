362295f9bbcb11ac25792dc9cad545a3e21c870f
unknown <information@tono-n-chi.com>
2017-07-10 18:59:49 +0900

    crawl

--- introducing_python/crawl.py
+++ introducing_python/crawl.py
@@ -6,21 +6,33 @@ from bs4 import BeautifulSoup
 pages = set()
 pagesAll = set()
 
+baseUrl = 'https://www.akita-bank.co.jp/'
+top = 'index.htm'
+
 
 def getLinks(pageUrl, depth):
     # print(' ' * depth, "searching:", pageUrl)
     global pages
     global pagesAll
-    html = urlopen(pageUrl)
+    try:
+        html = urlopen(pageUrl)
+    except:
+        print(' ' * depth, "Cannot open url:", pageUrl)
+        return
     bsObj = BeautifulSoup(html, "html.parser")
     try:
-        print(' ' * depth, bsObj.h1.get_text())
-    except AttributeError:
+        title = bsObj.findAll('title')
+        if title is None:
+            title = bsObj.h1.get_text()
+        else:
+            title = title[0].text
+        print(' ' * depth, title)
+    except:
         pass
         # print("This page is missing something! No worries though!")
 
     local_pages = set()
-    for link in bsObj.findAll("a", href=re.compile("^(/.*/|http://www.daisanbank.co.jp/.+)$")):
+    for link in bsObj.findAll("a", href=re.compile("^(/.*/|" + baseUrl + ".+)$")):
         if 'href' in link.attrs:
             if link.attrs['href'] not in pages:
                 if link.attrs['href'] not in pagesAll:
@@ -28,7 +40,7 @@ def getLinks(pageUrl, depth):
                     local_pages.add(link.attrs['href'])
                 pagesAll.add(link.attrs['href'])
 
-    for link in bsObj.findAll("a", href=re.compile("^(http://www.daisanbank.co.jp/.+)$")):
+    for link in bsObj.findAll("a", href=re.compile("^(" + baseUrl + ".+)$")):
         if 'href' in link.attrs:
             if link.attrs['href'] not in pages:
                 if link.attrs['href'] in pagesAll and link.attrs['href'] not in local_pages:
@@ -51,11 +63,11 @@ def getLinks(pageUrl, depth):
                 newPage = link.attrs['href']
                 print(' ' * depth, "----------------\n", ' ' * depth, newPage)
                 pages.add(newPage)
-                getLinks('http://www.daisanbank.co.jp/' + newPage, depth + 1)
+                getLinks(baseUrl + newPage, depth + 1)
 
 
 def ignore(url):
-    if re.match(".*tenpo-.*", url):
+    if re.match(".*tenpo.*", url):
         return True
     if re.match(".*kensaku.*", url):
         return True
@@ -66,4 +78,4 @@ def ignore(url):
     return False
 
 
-getLinks("http://www.daisanbank.co.jp/index.html", 0)
+getLinks(baseUrl + top, 0)
